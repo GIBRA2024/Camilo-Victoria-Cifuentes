92a4b039f7cb11eb8fea35360faf7cd6162c0246
Mateo Monroy <monroymateo1@gmail.com>
2025-02-08 22:06:56 -0500

    cambio de nombre

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function descargarContacto() {
     const vcardData = `BEGIN:VCARD
 VERSION:3.0
+N:Victoria Cifuentes;Camilo J.;;;
 FN:Camilo J. Victoria Cifuentes
 TEL: +57 3005114223
 NOTE: Negocios Corporativos 
-NOTE: camilo.victoria@jimenezpuerta.com
+EMAIL:camilo.victoria@jimenezpuerta.com
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
